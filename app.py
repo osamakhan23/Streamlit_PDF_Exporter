fc05d65f29faeaa138201878d69e896b98896cb9
Osama <osamakhanmm17@gmail.com>
2023-12-14 14:35:54 +1300

    second push

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 import io
+from io import BytesIO
+
+# Function to highlight text
+def highlight_text(page_text, search_query):
+    highlighted = page_text.replace(search_query, f"**{search_query}**")
+    return highlighted
 
 st.title("PDF Extractor App")
 
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+search_query = st.text_input("Enter text to search in the PDF")
+
 if uploaded_file is not None:
-    # Updated to use PdfReader instead of PdfFileReader
     pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
-    st.write("Number of pages:", len(pdf_reader.pages))
-    page_number = st.number_input("Page number", 1, len(pdf_reader.pages)) - 1
-    page_contents = pdf_reader.pages[page_number].extract_text()
-    st.write(page_contents)
+    page_number = st.number_input("Page number", 1, len(pdf_reader.pages), format='%i') - 1
+    page = pdf_reader.pages[page_number]
+    page_text = page.extract_text()
+
+    # Search functionality
+    if search_query:
+        page_text = highlight_text(page_text, search_query)
+
+    st.write(page_text)
+
+    # Prepare text for download
+    text_io = BytesIO(page_text.encode('utf-8'))  # Convert page text to BytesIO for download
+
+    # Download extracted text
+    if st.button("Download Text"):
+        st.download_button(label="Download Text as TXT",
+                           data=text_io.getvalue(),
+                           file_name="extracted_text.txt",
+                           mime="text/plain")
